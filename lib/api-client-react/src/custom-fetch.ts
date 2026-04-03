8d89e000ff7cc528fc0d807f44160ae3861ee6ab
asamani092-ux <a.samani092@gmail.com>
2026-04-03 21:34:32 +0300

    Update custom-fetch.ts

--- lib/api-client-react/src/custom-fetch.ts
+++ lib/api-client-react/src/custom-fetch.ts
@@ -15,7 +15,6 @@ const DEFAULT_JSON_ACCEPT = "application/json, application/problem+json";
 // Module-level configuration
 // ---------------------------------------------------------------------------
 
-من نهاية الرابط لكي لا يتكرر
 let _baseUrl: string | null = (import.meta.env.VITE_API_URL as string) || "https://tharaa-api.a-samani092.workers.dev";
 /**
  * Set a base URL that is prepended to every relative request URL
