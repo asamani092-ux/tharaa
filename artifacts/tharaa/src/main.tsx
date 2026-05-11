ad892fe3bb2db7aa4902886c894a043fd0e458d7
asamani092-ux <a.samani092@gmail.com>
2026-05-11 05:12:20 +0300

    Update main.tsx

--- artifacts/tharaa/src/main.tsx
+++ artifacts/tharaa/src/main.tsx
@@ -1,5 +1,23 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Toaster } from "sonner";
+import React from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Toaster
+      richColors
+      dir="rtl"
+      position="top-center"
+      toastOptions={{
+        classNames: {
+          toast: "font-sans",
+          title: "font-sans",
+          description: "font-sans",
+        },
+      }}
+    />
+  </React.StrictMode>
+);
