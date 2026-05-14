8d555d4012815bbfb0a492f817ab0f0771e74683
asamani092-ux <a.samani092@gmail.com>
2026-05-14 21:22:43 +0300

    Update main.tsx

--- artifacts/tharaa/src/main.tsx
+++ artifacts/tharaa/src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "@fontsource/ibm-plex-sans-arabic/400.css";
+import "@fontsource/ibm-plex-sans-arabic/500.css";
+import "@fontsource/ibm-plex-sans-arabic/600.css";
+import "@fontsource/ibm-plex-sans-arabic/700.css";
 import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
